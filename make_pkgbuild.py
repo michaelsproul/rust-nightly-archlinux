2db691c1b2503ca05d16079c0f4d75b49dec2b5c
Michael Sproul <micsproul@gmail.com>
2014-07-24 12:20:26 +1000

    Abandon tabs for indenting...
    I'm slowly going over to the dark side.

--- make_pkgbuild.py
+++ make_pkgbuild.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-import argparse
 import re
 import os
 import sys
+import argparse
 import datetime
 
 from sys import stderr
@@ -13,56 +13,57 @@ release_number_regex = r"CFG_RELEASE_NUM[ ]*=[ ]*(?P<value>.*)"
 release_label_regex = r"CFG_RELEASE_LABEL[ ]*=[ ]*(?P<value>.*)"
 
 def build_args():
-    parser = argparse.ArgumentParser(
-	    description='PKGBUILD generator for Rust nightly')
+    parser = argparse.ArgumentParser(description='PKGBUILD generator for Rust nightly')
     parser.add_argument('template',
-			type=str,
-			help='path to PKGBUILD template',
-			metavar='pkgbuild_template',
-			)
+                        type=str,
+                        help='Path to PKGBUILD template',
+                        metavar='pkgbuild_template',
+    )
     parser.add_argument('makefile',
-			type=str,
-			help='path to Rust makefile',
-			metavar='rust_makefile',
-			)
+                        type=str,
+                        help='Path to Rust makefile',
+                        metavar='rust_makefile',
+    )
     return parser
 
 def main():
-	args = build_args().parse_args()
+    args = build_args().parse_args()
 
-	# Extract the version information from the Rust makefile
-	with open(args.makefile, "r") as f:
-		rust_mk_contents = f.readlines()
+    # Extract the version information from the Rust makefile
+    with open(args.makefile, "r") as f:
+        rust_mk_contents = f.readlines()
 
-	r1 = re.compile(release_number_regex)
-	r2 = re.compile(release_label_regex)
+    r1 = re.compile(release_number_regex)
+    r2 = re.compile(release_label_regex)
 
-	version_number = None
-	version_label = None
+    version_number = None
+    version_label = None
 
-	for line in rust_mk_contents:
-		if version_number is not None and version_label is not None:
-			break
+    for line in rust_mk_contents:
+        if version_number is not None and version_label is not None:
+            break
 
-		m1 = r1.match(line)
-		if m1:
-			version_number = m1.group("value")
-			continue
+        m1 = r1.match(line)
+        if m1:
+            version_number = m1.group("value")
+            continue
 
-		m2 = r2.match(line)
-		if m2:
-			version_label = m2.group("value").replace("-", "_")
+        m2 = r2.match(line)
+        if m2:
+            version_label = m2.group("value").replace("-", "_")
 
-	datestring = date.today().strftime("%Y.%m.%d")
+    datestring = date.today().strftime("%Y.%m.%d")
 
-	version = version_number + version_label + "_" + datestring
+    version = version_number + version_label + "_" + datestring
 
-	# Write the PKGBUILD to stdout
-	with open(args.template, "r") as f:
-		pkgbuild = f.read()
+    # Write the PKGBUILD to stdout
+    with open(args.template, "r") as f:
+        pkgbuild = f.read()
 
-	pkgbuild = pkgbuild.replace("{VERSION}", version)
-	sys.stdout.write(pkgbuild)
+    pkgbuild = pkgbuild.replace("{VERSION}", version)
+    sys.stdout.write(pkgbuild)
 
 if __name__ == "__main__":
-	main()
+    main()
+
+# vim: set tabstop=4 shiftwidth=4 expandtab smarttab:
